867a9d908b9d918d8cfebba456b3662fa49613c1
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2022-08-20 20:43:44 -0400

    Updated for higher efficiency

--- figure-solver.py
+++ figure-solver.py
@@ -9,17 +9,17 @@ game_input = "cttdc" + "sccct" + "tcssd" + "tdctt" + "ddttd"
 
 # Code starts here.
 grid = {divmod(i, DIM): k for i, k in enumerate(game_input)}
-print(f"------------------------------------------------\nGame input below. {MAX_MOVES} moves allowed.")
+print(f"------------------------------------------------\nSolving the figure below in {MAX_MOVES} moves or fewer.")
 M = {"c": "◯", "t": "△", "s": "□", "d": "◇"}
 for row in range(DIM - 1, -1, -1):
     print(f"{row+1}\t", end="")
     for col in range(DIM):
         print(M[grid[(row, col)]], end="\t")
     print()
-print("\t1\t2\t3\t4\t5\n------------------------------------------------")
+print("\n\t1\t2\t3\t4\t5\n------------------------------------------------")
 
 
-def elim(col, g=grid):
+def elim(col, g):
     """eliminate the area associated with the selected column."""
     togo = {(0, col)}
     been = {(0, col)}
@@ -45,19 +45,26 @@ def elim(col, g=grid):
 
 def solve(g, moves):
     """recursive solver"""
+    global DIM, N, S
     if len(moves) > MAX_MOVES:
-        return (moves, False)
-    if not g:
-        return (moves, True)
+        return
+    elif not g:
+        N += 1
+        print(f"Solution {N}:\t{moves}")
+        return
+    prev = None  # to skip equivalent solutions
     for col in range(DIM):
-        if (0, col) in g:
-            r = solve(elim(col, g), moves + [col])
-            if r[1]:
-                return r
-    return (moves, False)
+        if (0, col) in g and g[(0, col)] != prev:
+            prev = g[(0, col)]
+            solve(elim(col, g), moves + (col,))
+    return
 
 
+N = 0
 t = perf_counter()
-S = solve(grid, [])[0]
+solve(grid, tuple())
 t = perf_counter() - t
-print(f"Solved in {t:.2f} seconds: {[i+1 for i in S]}\n------------------------------------------------")
+print(
+    f"\n------------------------------------------------\n{N} solutions found in {t:.2f} seconds."
+    "\n------------------------------------------------\n"
+)
